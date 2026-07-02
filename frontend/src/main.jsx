b61149cb2bf43e534539e3a69839cc45e452c65f
Aditi M Jambha <aditijambha@gmail.com>
2026-07-02 13:41:09 +0530

    Configure firebase project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+
 import "./index.css";
 
+import "./firebase/firebase";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
+
     <React.StrictMode>
+
         <App />
+
     </React.StrictMode>
+
 );
